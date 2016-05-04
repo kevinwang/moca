3818f92dc85671cd373d697f68c426a9d11f5d2d
Kevin Wang <kevin@kevinwang.com>
2016-05-04 00:14:13 -0500

    Set up database connections and retrieve lecture data.

--- README.md
+++ README.md
@@ -2,9 +2,26 @@
 CS 410 group project: MOOC Course Assistant
 
 ## Setup
+First, set up and import MOOC SQL databases:
+
+```bash
+echo "CREATE DATABASE textretrieval;" | mysql -u root
+mysql -u root textretrieval < Text\ Retrieval\ and\ Search\ Engines\ \(textretrieval-001\)_SQL_anonymized_general.sql
+
+echo "CREATE DATABASE textanalytics;" | mysql -u root
+mysql -u root textanalytics < Text\ Mining\ and\ Analytics\ \(textanalytics-001\)_SQL_anonymized_general.sql
+```
+
+Next, set up Python app:
+
 ```bash
 virtualenv venv
 source venv/bin/activate
 pip install -r requirements.txt
+```
+
+Finally, to run the app:
+
+```bash
 python app.py
 ```
--- app.py
+++ app.py
@@ -1,28 +1,68 @@
-from flask import Flask, redirect, render_template, url_for
+from flask import Flask, redirect, render_template, abort
+from functools import wraps
+from sqlalchemy import create_engine, MetaData, Table, select
 
 app = Flask(__name__, static_url_path='')
 app.debug = True
 
+course_ids = ['textretrieval', 'textanalytics']
+engines = {course_id: create_engine('mysql://root@localhost/' + course_id, echo=True) for course_id in course_ids}
+metadata = {course_id: MetaData(engine) for course_id, engine in engines.iteritems()}
+
+def validate_course(f):
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        if 'course_id' in kwargs and kwargs['course_id'] not in course_ids:
+            abort(404)
+        return f(*args, **kwargs)
+    return decorated_function
+
 @app.route('/')
-def home():
-    return redirect('/course/textretrieval-001')
+def route_home():
+    return render_template('home.html')
 
 @app.route('/coursestest')
-def coursetest():
+def route_coursetest():
     return render_template('specific.html')
 
 
-@app.route('/course/<course_id>')
-def course(course_id):
+@app.route('/<course_id>')
+@validate_course
+def route_course(course_id):
+    connection = engines[course_id].connect()
+    lecture_metadata = Table('lecture_metadata', metadata[course_id], autoload=True)
+    # TODO: Sort properly by x.y lecture number so 1.10 doesn't come before 1.2
+    s = (select([lecture_metadata.c.id, lecture_metadata.c.title])
+         .where((lecture_metadata.c.parent_id == -1) & (lecture_metadata.c.deleted == 0))
+         .order_by(lecture_metadata.c.title))
+    result = connection.execute(s)
+    connection.close()
+
+    lectures = [{'id': lecture[0], 'title': lecture[1]} for lecture in result]
+
     hardest_topics = ['hardest', 'harder', 'hard']
     easiest_topics = ['easiest', 'easier', 'easy']
-    videos = ['1.1', '1.2', '1.3']
+
     return render_template(
             'course.html',
             course_id=course_id,
             hardest_topics=hardest_topics,
             easiest_topics=easiest_topics,
-            videos=videos)
+            lectures=lectures)
+
+@app.route('/<course_id>/lecture/<int:lecture_id>')
+@validate_course
+def route_lecture(course_id, lecture_id):
+    connection = engines[course_id].connect()
+    lecture_metadata = Table('lecture_metadata', metadata[course_id], autoload=True)
+    s = (select([lecture_metadata.c.id, lecture_metadata.c.title])
+         .where(lecture_metadata.c.id == lecture_id)
+         .limit(1))
+    lecture = list(connection.execute(s))[0]
+    connection.close()
+
+    lecture = {'id': lecture[0], 'title': lecture[1]}
+    return render_template('lecture.html', lecture=lecture)
 
 if __name__ == '__main__':
     app.run()
